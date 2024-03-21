9e667ebf0bf57954f3bd54c678a555ae00d6aea4
Joeytt-W <49783416+Joeytt-W@users.noreply.github.com>
2024-03-21 22:49:59 +0800

    随机数小程序

--- Rust/Codes/guessing_game/src/main.rs
+++ Rust/Codes/guessing_game/src/main.rs
@@ -1,14 +1,30 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("猜数");
+    let secret_number = rand::thread_rng().gen_range(1..=101); //随机数生成器
+    loop {
+        println!("猜测一个数");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("无法读取行");
+        //let guess: u32 = guess.trim().parse().expect("请输入一个数字");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        /*
+        print!("你猜测的数是:{}",guess);
+        */
 
-    println!("猜测一个数");
-
-    let mut guess = String::new();
-
-    io::stdin().read_line(&mut guess)
-        .expect("无法读取行");
-
-    print!("你猜测的数是:{}",guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small"),
+            Ordering::Greater => println!("Too Big"),
+            Ordering::Equal => {
+                println!("You Win!");
+                break;
+            }
+        }
+    }
 }
---
+++ Rust/Codes/guessing_game/target/debug/deps/guessing_game-33ca309d430af64d.d
@@ -0,0 +1,5 @@
+D:\WorkSpaces\Self\Note\Rust\Codes\guessing_game\target\debug\deps\libguessing_game-33ca309d430af64d.rmeta: src\main.rs
+
+D:\WorkSpaces\Self\Note\Rust\Codes\guessing_game\target\debug\deps\guessing_game-33ca309d430af64d.d: src\main.rs
+
+src\main.rs:
